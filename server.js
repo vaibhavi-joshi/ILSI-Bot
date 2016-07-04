dd0064da6a60ebaa9e8250f8eab2f81b384b9b0e
vaibhavi.joshi <vaibhavi.joshi@infostretch.com>
2016-07-04 19:56:53 +0530

    modify code to botconnectorbot

--- server.js
+++ server.js
@@ -1,30 +1,15 @@
 const restify = require('restify');
-const skype = require('skype-sdk');
 const builder = require('botbuilder');
 
-APP_ID = "63f831ed-4469-4453-8330-d92d9929449d"
-APP_SECRET = "YszUtpojbuhVThz5hxGNzAx"
-
-// Initialize the BotService
-const botService = new skype.BotService({
-    messaging: {
-        botId: "63f831ed-4469-4453-8330-d92d9929449d",
-        serverUrl : "https://apis.skype.com",
-        requestTimeout : 15000,
-        appId: "63f831ed-4469-4453-8330-d92d9929449d",
-        appSecret: "YszUtpojbuhVThz5hxGNzAx"
-    }
-});
-
 // Create bot and add dialogs
-var bot = new builder.SkypeBot(botService);
+var bot = new builder.BotConnectorBot({ appId: 'ILSI', appSecret: '6bc9fdec647a44fe8847447e78096758' });
 bot.add('/', function (session) {
    session.send('Hello World'); 
 });
 
 // Setup Restify Server
-const server = restify.createServer();
-server.post('/v1/chat', skype.messagingHandler(botService));
-server.listen(process.env.PORT || 8081, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+var server = restify.createServer();
+server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+server.listen(process.env.port || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url); 
 });
