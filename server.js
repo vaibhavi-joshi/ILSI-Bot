9a118d6d22241de3b8bed8131fc66478750c1be8
vaibhavi.joshi <vaibhavi.joshi@infostretch.com>
2016-07-05 18:05:58 +0530

    LUISE

--- server.js
+++ server.js
@@ -4,9 +4,16 @@ var builder = require('botbuilder');
 // Create bot and add dialogs
 //var bot = new builder.TextBot();
 var bot = new builder.BotConnectorBot();
-bot.add('/', function (session) {
-   session.send('Hello World'); 
-});
+
+
+//https://api.projectoxford.ai/luis/v1/application?id=c413b2ef-382c-45bd-8ff0-f76d60e2a821&subscription-key=0d6df140e73b4f07a204058a0769d60e&q=When%20is%20next%20holiday
+var model = 'https://api.projectoxford.ai/luis/v1/application?id=814f9a05-0e84-41f4-aec9-f205211b3a46&subscription-key=0d6df140e73b4f07a204058a0769d60e&q=';
+var dialog = new builder.LuisDialog(model);
+
+bot.add('/', dialog);
+
+dialog.on('CheckInHRMS', builder.DialogAction.send("Next Holiday is 'Independece Day' on Monday July 04 2016"));
+dialog.onDefault(builder.DialogAction.send("I'm sorry. I didn't understand."));
 
 // Setup Restify Server
 
@@ -17,3 +24,6 @@ server.post('/v1/messages', bot.listen());
 server.listen(8081, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
+
+
+//bot.listenStdin()
