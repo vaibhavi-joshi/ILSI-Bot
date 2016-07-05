b9f712a4df93aa92905c8b6f015b444ebd973add
vaibhavi.joshi <vaibhavi.joshi@infostretch.com>
2016-07-05 11:36:17 +0530

    agian write the code

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
-const restify = require('restify');
-const builder = require('botbuilder');
+var restify = require('restify');
+var builder = require('botbuilder');
 
 // Create bot and add dialogs
 var bot = new builder.BotConnectorBot();
 bot.add('/', function (session) {
-   session.send('Hello! I am connected'); 
+   session.send('Hello World'); 
 });
 
 // Setup Restify Server
 
-// server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
-// server.listen(process.env.port || 8085, function () {
-//     console.log('%s listening to %s', server.name, server.url); 
-// });
 var server = restify.createServer();
-server.use(bot.verifyBotFramework({ appId: 'ILSIBOT', appSecret: 'bdb784cb093a461a858b2db0f6ba62b5' }));
+server.use(bot.verifyBotFramework({appId:'ILSIBOT', appSecret:'bdb784cb093a461a858b2db0f6ba62b5' }));
 server.post('/v1/messages', bot.listen());
 
-server.listen(8085, function () {
+server.listen(8081, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
