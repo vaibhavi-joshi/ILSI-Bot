8f4b2db4a77ef3d8dc9078f8a6999e4490f67b4c
vaibhavijoshi <jvaibhavi600@gmail.com>
2016-07-05 11:49:11 +0530

    update code
    As per http://docs.botframework.com/builder/node/bots/BotConnectorBot/#navtitle guideline

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var restify = require('restify');
 var builder = require('botbuilder');
 
 // Create bot and add dialogs
-var bot = new builder.BotConnectorBot();
+var bot = new builder.BotConnectorBot({ appId: 'ILSIBOT', appSecret: 'bdb784cb093a461a858b2db0f6ba62b5' });
 bot.add('/', function (session) {
    session.send('Hello World'); 
 });
@@ -10,9 +10,7 @@ bot.add('/', function (session) {
 // Setup Restify Server
 
 var server = restify.createServer();
-server.use(bot.verifyBotFramework({appId:'ILSIBOT', appSecret:'bdb784cb093a461a858b2db0f6ba62b5' }));
-server.post('/v1/messages', bot.listen());
-
-server.listen(8081, function () {
+server.post('/v1/messages', bot.verifyBotFramework(), bot.listen());
+server.listen(process.env.port || 3978, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
