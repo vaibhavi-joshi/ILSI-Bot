9d396224c80af7bfcc4f3c1d3e62a5674fb79a07
vaibhavi.joshi <vaibhavi.joshi@infostretch.com>
2016-07-11 14:39:38 +0530

    did minor changes

--- server.js
+++ server.js
@@ -19,6 +19,96 @@ var username = null
 var password = null
 var emp_number = null
 var current_leave_count = null
+bot.add('/', dialog);
+
+
+bot.add('/login',[ function (session) {
+        username = null
+        password = null
+		builder.Prompts.text(session, 'Enter your username and password for login. First Enter your username.');
+	},
+	function (session, results, next) {
+		username = results.response;
+	//	console.log('username = %s',username);
+		builder.Prompts.text(session, 'Enter your Domain Password');
+	},
+	function (session, results, next) {
+		password = results.response;
+        session.send('Please wait... I am logging in for you...');
+		//console.log('passowrd = %s',password);
+	
+
+        //Call login API of HRMS with user Input
+        login(username, password,function (token, emp_num, err) {
+		
+			if(token == null) {
+				builder.Prompts.choice(session, "Something went wrong while logging in. Retry?", ["yes", "no"]);
+                 next();
+			}
+			else {
+				
+				//set received token and emp_num to program vars for future work
+				sessionID = token;
+				emp_number = emp_num;
+                session.send('You are now logged into HRMS');
+                session.endDialog(); //For BotconnectorBot
+			  //session.replaceDialog('/'); //For Textbot to test scenario
+			}
+		}); 
+       
+ },
+ function (session, results, next) {
+		if(results.response) {
+			var choice = results.response.entity;
+			//console.log('choice entered = %s', choice);
+			if(choice == "yes") {
+				//session.endDialog();
+				session.beginDialog("/login");
+			}
+			else {
+				session.endDialog();		
+			}
+		}
+       	
+	}
+ 
+ ]);
+ 
+ 
+ 
+ // Intent Handlers
+dialog.on('CheckLeave', [function (session) {
+
+if (sessionID != null && emp_number != null) {
+		    session.send("your previous session is still alive, you can continue with HRMS Task..ask for check leave balance");
+            checkLeaveBalance(function (leave_count,error) {
+
+					if (error == null)
+					{
+						session.send('Your available leave balance is = %s',leave_count)
+					}
+					else
+					{
+							session.send('Something went wrong while checking for your leave balance, please try after sometime.');
+					}
+			});
+		}//end elseif
+		else {
+			session.beginDialog('/login'); //do not have token so ask to login
+          
+        }
+}]
+
+
+);
+
+dialog.on('CheckHoliday', function (session, args) {
+
+    session.send("Next Holiday is 'Independece Day' on Monday July 04 2016");
+});
+dialog.onDefault(builder.DialogAction.send("I'm sorry. I didn't understand."));
+
+// functions
 
 function login(uname, pwd,callback) {
 
@@ -75,97 +165,7 @@ request(options, function (error, response, body) {
 	
 });
 
-
 }
-bot.add('/', dialog);
-
-bot.add('/login',[ function (session) {
-        username = null
-        password = null
-		builder.Prompts.text(session, 'Enter your username and password for login. First Enter your username.');
-	},
-	function (session, results, next) {
-		username = results.response;
-	//	console.log('username = %s',username);
-		builder.Prompts.text(session, 'Enter your Domain Password');
-	},
-	function (session, results, next) {
-		password = results.response;
-        session.send('Please wait... I am logging in for you...');
-		//console.log('passowrd = %s',password);
-	
-
-        //Call login API of HRMS with user Input
-        login(username, password,function (token, emp_num, err) {
-		
-			if(token == null) {
-				builder.Prompts.choice(session, "Something went wrong while logging in. Retry?", ["yes", "no"]);
-                 next();
-			}
-			else {
-				
-				//set received token and emp_num to program vars for future work
-				sessionID = token;
-				emp_number = emp_num;
-                session.send('You are now logged into HRMS');
-                session.endDialog(); //For BotconnectorBot
-			  //session.replaceDialog('/'); //For Textbot to test scenario
-			}
-		}); 
-       
- },
- function (session, results, next) {
-		if(results.response) {
-			var choice = results.response.entity;
-			//console.log('choice entered = %s', choice);
-			if(choice == "yes") {
-				//session.endDialog();
-				session.beginDialog("/login");
-			}
-			else {
-				session.endDialog();		
-			}
-		}
-       	
-	}
- 
- ]);
-// Intent Handler "CheckInHRMS" and "onDefault"
-
-
-
-dialog.on('CheckLeave', [function (session) {
-
-if (sessionID != null && emp_number != null) {
-		    session.send("your previous session is still alive, you can continue with HRMS Task..ask for check leave balance");
-            checkLeaveBalance(function (leave_count,error) {
-
-					if (error == null)
-					{
-						session.send('Your available leave balance is = %s',leave_count)
-					}
-					else
-					{
-							session.send('Something went wrong while checking for your leave balance, please try after sometime.');
-					}
-			});
-		}//end elseif
-		else {
-			session.beginDialog('/login'); //do not have token so ask to login
-          
-        }
-}]
-
-
-);
-
-dialog.on('CheckHoliday', function (session, args) {
-
-    session.send("Next Holiday is 'Independece Day' on Monday July 04 2016");
-});
-dialog.onDefault(builder.DialogAction.send("I'm sorry. I didn't understand."));
-
-
 
 // Setup Restify Server
 
