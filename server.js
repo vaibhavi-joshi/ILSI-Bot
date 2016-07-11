7d2d8d3441a4e5165e6d4e3e8cadaede6f7c477a
vaibhavi.joshi <vaibhavi.joshi@infostretch.com>
2016-07-11 18:04:36 +0530

    chnage port num

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var request = require('request');
 
 //var bot = new builder.TextBot();
 var bot = new builder.BotConnectorBot();
-var port = process.env.PORT || 8081;
+var port = process.env.PORT || 8085;
 var model = 'https://api.projectoxford.ai/luis/v1/application?id=814f9a05-0e84-41f4-aec9-f205211b3a46&subscription-key=0d6df140e73b4f07a204058a0769d60e';
 var dialog = new builder.LuisDialog(model);
 
