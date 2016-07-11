8406c38b7086fb3f8914d0dcbf23e9cde567ca8f
vaibhavi.joshi <vaibhavi.joshi@infostretch.com>
2016-07-11 17:52:55 +0530

    added url

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var request = require('request');
 // Create bot and add dialogs
 
 //var bot = new builder.TextBot();
-var bot = new builder.BotConnectorBot();
+var bot = new builder.BotConnectorBot{ appId: 'ILSIBOT', appSecret: 'bdb784cb093a461a858b2db0f6ba62b5' }();
 var port = process.env.PORT || 8081;
 var model = 'https://api.projectoxford.ai/luis/v1/application?id=814f9a05-0e84-41f4-aec9-f205211b3a46&subscription-key=0d6df140e73b4f07a204058a0769d60e';
 var dialog = new builder.LuisDialog(model);
