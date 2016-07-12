2e9c3192ba7cff87717d39ea9cf5a7bcd8aaa395
vaibhavi.joshi <vaibhavi.joshi@infostretch.com>
2016-07-12 14:13:47 +0530

    chnage restify code

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
  var request = require('request');
  
 
- var bot = new builder.BotConnectorBot();
+ var bot = new builder.BotConnectorBot({appId:'ILSIBOT', appSecret:'bdb784cb093a461a858b2db0f6ba62b5' });
  //var bot = new builder.TextBot();
  
  //https://api.projectoxford.ai/luis/v1/application?id=c413b2ef-382c-45bd-8ff0-f76d60e2a821&subscription-key=0d6df140e73b4f07a204058a0769d60e&q=When%20is%20next%20holiday
@@ -22,6 +22,8 @@ var current_leave_count = null
  
  bot.add('/', dialog);
 
+ 
+
  dialog.on('CheckHoliday',function (session, args) {
 
 	console.log('holiday detected')
@@ -33,13 +35,17 @@ var current_leave_count = null
 //**************************************** Restify server *******************************************************
  var server = restify.createServer();
 
-server.use(bot.verifyBotFramework({appId:'ILSIBOT', appSecret:'bdb784cb093a461a858b2db0f6ba62b5' }));
-server.post('/v1/messages', bot.listen());
+// server.use(bot.verifyBotFramework());
+// server.post('/v1/messages', bot.listen());
 
-server.listen(8081, function () {
+// server.listen(8081, function () {
+//     console.log('%s listening to %s', server.name, server.url); 
+// });
+
+server.post('/v1/messages', bot.verifyBotFramework(), bot.listen());
+server.listen(process.env.port || 8081, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
-
 
  //bot.listenStdin()
 
