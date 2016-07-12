edabcb7566c06394fa545b39edd8330e68716757
vaibhavi.joshi <vaibhavi.joshi@infostretch.com>
2016-07-12 12:58:57 +0530

    app id and scret

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
  var request = require('request');
  
 
- var bot = new builder.BotConnectorBot();
+ var bot = new builder.BotConnectorBot({ appId: 'ILSIBOT', appSecret: 'bdb784cb093a461a858b2db0f6ba62b5' });
  //var bot = new builder.TextBot();
  
  //https://api.projectoxford.ai/luis/v1/application?id=c413b2ef-382c-45bd-8ff0-f76d60e2a821&subscription-key=0d6df140e73b4f07a204058a0769d60e&q=When%20is%20next%20holiday
