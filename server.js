09dad43a27a1e66b3c8bf5a677c01ea76186377b
vaibhavi.joshi <vaibhavi.joshi@infostretch.com>
2016-07-12 13:58:06 +0530

    remove &q= from luis model url

--- server.js
+++ server.js
@@ -7,7 +7,7 @@
  //var bot = new builder.TextBot();
  
  //https://api.projectoxford.ai/luis/v1/application?id=c413b2ef-382c-45bd-8ff0-f76d60e2a821&subscription-key=0d6df140e73b4f07a204058a0769d60e&q=When%20is%20next%20holiday
- var model = 'https://api.projectoxford.ai/luis/v1/application?id=814f9a05-0e84-41f4-aec9-f205211b3a46&subscription-key=0d6df140e73b4f07a204058a0769d60e&q=';
+ var model = 'https://api.projectoxford.ai/luis/v1/application?id=814f9a05-0e84-41f4-aec9-f205211b3a46&subscription-key=0d6df140e73b4f07a204058a0769d60e';
  var dialog = new builder.LuisDialog(model);
 
 var host = "http://10.12.40.86"
