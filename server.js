edac19cbec3fb0ac73cfbe4c3ab340193da5c41f
vaibhavijoshi <jvaibhavi600@gmail.com>
2016-07-12 10:51:52 +0530

    revert to commit no 8578a0

--- server.js
+++ server.js
@@ -1,35 +1,29 @@
-var restify = require('restify');
-var builder = require('botbuilder');
+ var restify = require('restify');
+ var builder = require('botbuilder');
+ 
 
+ var bot = new builder.BotConnectorBot();
+ 
+ 
+ //https://api.projectoxford.ai/luis/v1/application?id=c413b2ef-382c-45bd-8ff0-f76d60e2a821&subscription-key=0d6df140e73b4f07a204058a0769d60e&q=When%20is%20next%20holiday
+ var model = 'https://api.projectoxford.ai/luis/v1/application?id=814f9a05-0e84-41f4-aec9-f205211b3a46&subscription-key=0d6df140e73b4f07a204058a0769d60e&q=';
+ var dialog = new builder.LuisDialog(model);
+ 
+ bot.add('/', dialog);
+ 
+ dialog.on('CheckInHRMS', builder.DialogAction.send("Next Holiday is 'Independece Day' on Monday July 04 2016"));
+ dialog.onDefault(builder.DialogAction.send("I'm sorry. I didn't understand."));
+ 
 
-// Create bot and add dialogs
+ 
+ var server = restify.createServer();
 
-//var bot = new builder.TextBot();
-var bot = new builder.BotConnectorBot();
-var model = 'https://api.projectoxford.ai/luis/v1/application?id=814f9a05-0e84-41f4-aec9-f205211b3a46&subscription-key=0d6df140e73b4f07a204058a0769d60e&q=';
-var dialog = new builder.LuisDialog(model);
+server.post('/v1/messages', bot.verifyBotFramework(), bot.listen());
+server.listen(process.env.port || 8080, function () {
+     console.log('%s listening to %s', server.name, server.url); 
+ });
+ 
+ 
 
-
-bot.add('/', dialog);
-
-dialog.on('CheckHoliday', function (session, args) {
-
-    session.send("Next Holiday is 'Independece Day' on Monday July 04 2016");
-});
-dialog.onDefault(builder.DialogAction.send("I'm sorry. I didn't understand."));
-
-
-
-// Setup Restify Server
-
-var server = restify.createServer();
-
-server.use(bot.verifyBotFramework({appId:'ILSIBOT', appSecret:'bdb784cb093a461a858b2db0f6ba62b5' }));
-server.post('/v1/messages', bot.listen());
-
-var listener = server.listen(8080, function () {
-	console.log('server started on port %s', listener.address().port);
-});
-
-//bot.listenStdin()
+ //bot.listenStdin()
 
